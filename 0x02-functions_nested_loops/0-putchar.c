5f950c8710534f70b4a2bf95f04c047b03499584
abdelkarime <abdelkarime.mou@gmail.com>
2023-05-23 14:05:02 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
-#include "_putchar.h"
+#include <unistd.h>
 
 /**
   * main - Entry point
   *
   * Return: Always 0
   */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_putchar');
-	_putchar('\n');
 
-	return (0);
+	return (write(1,&c,1));
 }
