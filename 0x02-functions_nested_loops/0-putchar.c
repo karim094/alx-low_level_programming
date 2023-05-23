665cc0fd41f0f3460ee988bc307104571dbbacc8
abdelkarime <abdelkarime.mou@gmail.com>
2023-05-23 14:20:06 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
   * main - Entry point
   *
   * Return: Always 0
   */
-int _putchar(char c)
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	return (write(1,&c,1));
+	return (0);
 }
